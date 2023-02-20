d743e459d226d9ca5763659cfa9ea69a7d7acf01
Ozan <ozancangu24@gmail.com>
2023-02-20 23:58:11 +0300

    Revert "-image slider added"
    This reverts commit d1c915991e8f50e7b532c44f689d8d1fb8ef62c9.

--- app/src/main/java/com/ozancanguz/motivate_me_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ozancanguz/motivate_me_app/ui/home/HomeFragment.kt
@@ -5,11 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
 import com.ozancanguz.motivate_me_app.R
-import com.ozancanguz.motivate_me_app.data.adapters.ImageSliderAdapter
 import com.ozancanguz.motivate_me_app.databinding.FragmentHomeBinding
-import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
@@ -26,11 +23,6 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
           _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
-
-
-        val viewPager=binding.viewPager
-        val adapter = ImageSliderAdapter(listOf(R.drawable.nevergiveup, R.drawable.bekind, R.drawable.keep,R.drawable.mylogo))
-        viewPager.adapter = adapter
 
 
         return view
