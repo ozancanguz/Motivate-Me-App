46472a06d9607508c43db81da139854ccb2f50b4
Ozan <ozancangu24@gmail.com>
2023-02-20 20:36:18 +0300

    Initial commit

---
+++ app/src/main/java/com/ozancanguz/motivate_me_app/ui/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ozancanguz.motivate_me_app.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ozancanguz.motivate_me_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        
+
+    }
+}
