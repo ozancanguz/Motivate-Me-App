ee4b96d7c99f0486396cb9bff3d976aed76057e8
Ozan <ozancangu24@gmail.com>
2023-02-21 19:08:33 +0300

    -emoji picker added

--- app/src/main/java/com/ozancanguz/motivate_me_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ozancanguz/motivate_me_app/ui/home/HomeFragment.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.ozancanguz.motivate_me_app.R
 import com.ozancanguz.motivate_me_app.data.adapter.ImageSliderAdapter
+
 import com.ozancanguz.motivate_me_app.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
 
 
 class HomeFragment : Fragment() {
@@ -37,8 +40,12 @@ class HomeFragment : Fragment() {
         nav()
 
 
+        emojiPicker()
+
         return view
     }
+
+
 
     private fun nav() {
         binding.diarynav.setOnClickListener {
@@ -49,5 +56,68 @@ class HomeFragment : Fragment() {
         }
     }
 
+    private fun emojiPicker() {
 
-}
+        binding.feelingText.setOnClickListener {
+            val bottomSheet = BottomSheetDialog(requireContext())
+            bottomSheet.setContentView(R.layout.bottom_sheet_layout)
+
+
+            val emoji1 = bottomSheet.emoji1
+            val emoji2 = bottomSheet.emoji2
+            val emoji3 = bottomSheet.emoji3
+            val emoji4 = bottomSheet.emoji4
+            val emoji5 = bottomSheet.emoji5
+            val emoji6 = bottomSheet.emoji6
+            val emoji7 = bottomSheet.emoji7
+            val emoji8 = bottomSheet.emoji8
+
+            emoji1?.setOnClickListener {
+               binding.emojiText.setText("😀")
+                bottomSheet.dismiss()
+            }
+
+            emoji2?.setOnClickListener {
+                binding.emojiText.setText("😂")
+                bottomSheet.dismiss()
+            }
+
+            emoji3?.setOnClickListener {
+                binding.emojiText.setText("😍")
+                bottomSheet.dismiss()
+            }
+
+            emoji4?.setOnClickListener {
+                binding.emojiText.setText("🤔")
+                bottomSheet.dismiss()
+            }
+
+            emoji5?.setOnClickListener {
+                binding.emojiText.setText("😴")
+                bottomSheet.dismiss()
+            }
+
+            emoji6?.setOnClickListener {
+                binding.emojiText.setText("🤢")
+                bottomSheet.dismiss()
+            }
+
+            emoji7?.setOnClickListener {
+                binding.emojiText.setText("😭")
+                bottomSheet.dismiss()
+            }
+
+            emoji8?.setOnClickListener {
+                binding.emojiText.setText("🥳")
+                bottomSheet.dismiss()
+            }
+
+            bottomSheet.show()
+        }
+    }
+
+
+    }
+
+
+
