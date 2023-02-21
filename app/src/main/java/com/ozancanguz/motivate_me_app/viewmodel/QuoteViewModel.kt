4641f966327e5f375021238f5858ec6cb15769b7
Ozan <ozancangu24@gmail.com>
2023-02-21 19:26:10 +0300

    -Quoteviewmodel.kt created and request function created

---
+++ app/src/main/java/com/ozancanguz/motivate_me_app/viewmodel/QuoteViewModel.kt
@@ -0,0 +1,31 @@
+package com.ozancanguz.motivate_me_app.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ozancanguz.motivate_me_app.data.Repository
+import com.ozancanguz.motivate_me_app.data.models.Quote
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuoteViewModel@Inject constructor(private val repository: Repository, application: Application):AndroidViewModel(application) {
+
+    var quote=MutableLiveData<Quote>()
+
+    fun getQuote(){
+        viewModelScope.launch {
+            val response=repository.remote.getQuote()
+            if(response.isSuccessful){
+                quote.postValue(response.body())
+            }else{
+                Log.d("quoteviewmodel","No data for quote")
+            }
+        }
+    }
+
+
+}
