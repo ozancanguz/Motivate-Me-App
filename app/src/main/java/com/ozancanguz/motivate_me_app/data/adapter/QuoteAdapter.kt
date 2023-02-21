6698d01a14d21df3ed5d0337763e17b6ce0e9e39
Ozan <ozancangu24@gmail.com>
2023-02-21 19:33:16 +0300

    -Recyclerview adapter created

---
+++ app/src/main/java/com/ozancanguz/motivate_me_app/data/adapter/QuoteAdapter.kt
@@ -0,0 +1,41 @@
+package com.ozancanguz.motivate_me_app.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.motivate_me_app.R
+import com.ozancanguz.motivate_me_app.data.models.Quote
+import com.ozancanguz.motivate_me_app.data.models.QuoteItem
+import kotlinx.android.synthetic.main.quote_row_layout.view.*
+
+class QuoteAdapter:RecyclerView.Adapter<QuoteAdapter.MyViewHolder>() {
+    class MyViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+
+    }
+
+    var quoteList= emptyList<QuoteItem>()
+
+    fun setData(newData:Quote){
+        this.quoteList=newData
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.quote_row_layout,parent,false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentQuote=quoteList[position]
+        holder.itemView.quoteTextView.text=currentQuote.quote
+
+    }
+
+    override fun getItemCount(): Int {
+        return quoteList.size
+    }
+
+}
