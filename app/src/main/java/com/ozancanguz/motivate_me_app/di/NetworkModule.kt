a9f10e722ce462d220e138da11b0dcca8a10e81e
Ozan <ozancangu24@gmail.com>
2023-02-21 19:22:21 +0300

    -NetworkModule.kt created

---
+++ app/src/main/java/com/ozancanguz/motivate_me_app/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.ozancanguz.motivate_me_app.di
+
+
+import com.ozancanguz.motivate_me_app.data.api.QuoteApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+import javax.inject.Singleton
+import kotlin.time.DurationUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://zylalabs.com/api/936/motivational+phrases+api/754/")
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): QuoteApi {
+        return retrofit.create(QuoteApi::class.java)
+    }
+}
