147170ce1a9c7d7bcf70cec0b9644ce351df2465
Ozan <ozancangu24@gmail.com>
2023-02-22 19:45:16 +0300

    -Time added to rv

--- app/src/main/java/com/ozancanguz/motivate_me_app/ui/diary/AddDiaryFragment.kt
+++ app/src/main/java/com/ozancanguz/motivate_me_app/ui/diary/AddDiaryFragment.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.motivate_me_app.ui.diary
 
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +13,8 @@ import com.ozancanguz.motivate_me_app.data.models.diary.Diary
 import com.ozancanguz.motivate_me_app.databinding.FragmentAddDiaryBinding
 import com.ozancanguz.motivate_me_app.viewmodel.DiaryViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class AddDiaryFragment : Fragment() {
@@ -20,6 +23,8 @@ class AddDiaryFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val diaryViewModel:DiaryViewModel by viewModels()
+
+    private lateinit var title:String
 
 
 
@@ -34,7 +39,12 @@ class AddDiaryFragment : Fragment() {
 
 
 
+
+
+
         insertToDb()
+
+
 
 
         return view
@@ -42,11 +52,19 @@ class AddDiaryFragment : Fragment() {
     }
 
 
+
+
     fun insertToDb(){
         binding.addtoDb.setOnClickListener {
-            val title=binding.titleEt.text.toString()
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH) + 1 // Note: Month starts from 0
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val dateString = dateFormat.format(calendar.time)
+            val time= "$dayOfMonth-$month-$year"
             val description=binding.descriptionEt.text.toString()
-            var newData= Diary(0,title,description)
+            var newData= Diary(0,time,description)
             diaryViewModel.insertData(newData)
             findNavController().navigate(R.id.action_addDiaryFragment_to_diaryFragment)
         }
