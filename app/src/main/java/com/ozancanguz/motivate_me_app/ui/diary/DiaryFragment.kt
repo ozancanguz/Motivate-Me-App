27d3a84ae5015c8c236ba6e6954cbb8cc8fb0b7c
Ozan <ozancangu24@gmail.com>
2023-02-22 19:58:26 +0300

    -Swipe to delete added

--- app/src/main/java/com/ozancanguz/motivate_me_app/ui/diary/DiaryFragment.kt
+++ app/src/main/java/com/ozancanguz/motivate_me_app/ui/diary/DiaryFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.ozancanguz.motivate_me_app.R
 import com.ozancanguz.motivate_me_app.data.adapter.DiaryListAdapter
 import com.ozancanguz.motivate_me_app.databinding.FragmentDiaryBinding
+import com.ozancanguz.motivate_me_app.util.SwipeToDelete
 import com.ozancanguz.motivate_me_app.viewmodel.DiaryViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_diary.*
 
 @AndroidEntryPoint
 class DiaryFragment : Fragment() {
@@ -51,6 +56,7 @@ class DiaryFragment : Fragment() {
     private fun setupRv() {
         binding.diaryRv.layoutManager=LinearLayoutManager(requireContext())
         binding.diaryRv.adapter=listAdapter
+        swipeToDelete(binding.diaryRv)
     }
 
     private fun observeLiveData() {
@@ -67,6 +73,23 @@ class DiaryFragment : Fragment() {
             findNavController().navigate(R.id.action_diaryFragment_to_addDiaryFragment)
         }
     }
+
+    //swipe to delete
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+
+        val swipeToDeleteCallback=object : SwipeToDelete(){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemtoDelete=listAdapter.todolist[viewHolder.adapterPosition]
+                diaryViewModel.deleteSingleItem(itemtoDelete)
+                Toast.makeText(requireContext(),"Removed successfully", Toast.LENGTH_LONG).show()
+
+
+            }
+        }
+        val itemTouchHelper= ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
 
 
 }
