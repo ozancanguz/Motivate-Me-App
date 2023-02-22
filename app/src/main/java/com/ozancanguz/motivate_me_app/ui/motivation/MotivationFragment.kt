1e358b9c85b5353b8cd5da4e7b84f51f9e3cff4f
Ozan <ozancangu24@gmail.com>
2023-02-22 18:27:22 +0300

    -Diary fragment designed

--- app/src/main/java/com/ozancanguz/motivate_me_app/ui/motivation/MotivationFragment.kt
+++ app/src/main/java/com/ozancanguz/motivate_me_app/ui/motivation/MotivationFragment.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.motivate_me_app.ui.motivation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -75,6 +76,8 @@ class MotivationFragment : Fragment() {
 
 
 
+
+
         return view
 
     }
@@ -95,7 +98,5 @@ class MotivationFragment : Fragment() {
 
 
 
+    }
 
-
-
-}
