c33931c97247e331266c0acab534884ed3063627
Ozan <ozancangu24@gmail.com>
2023-02-22 18:36:59 +0300

    -Diary Database created

---
+++ app/src/main/java/com/ozancanguz/motivate_me_app/data/models/diary/DiaryDatabase.kt
@@ -0,0 +1,14 @@
+package com.ozancanguz.motivate_me_app.data.models.diary
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@Database(entities = [Diary::class], version = 1,
+    exportSchema = false,)
+
+@TypeConverters(TypeConverter::class)
+abstract class DiaryDatabase: RoomDatabase(){
+    abstract fun diaryDao():DiaryDao
+}
